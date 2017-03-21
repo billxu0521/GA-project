2d4afb2b7216171d52b179d76dac389fbc5ef2b2
billxu <billxu0521@gmail.com>
2017-03-21 22:59:36 +0800

    no message

--- config/www.travel.taipei.js
+++ config/www.travel.taipei.js
@@ -72,11 +72,11 @@ var exec = function () {
 
     //4-1  
     ga_mouse_click_event('.btn-radio',"GL4-1");
-    ga_mouse_click_event('.row condation-duration-blk js-condation-duration .year',"GL4-1");
-    ga_mouse_click_event('.row condation-duration-blk js-condation-duration .month',"GL4-1");
-    ga_mouse_click_event('.row condation-duration-blk js-condation-duration even .year',"GL4-1");
-    ga_mouse_click_event('.row condation-duration-blk js-condation-duration even .month',"GL4-1");
-
+    ga_mouse_click_event('#start-year',"GL4-1");
+    ga_mouse_click_event('#start-month',"GL4-1");
+    ga_mouse_click_event('#end-year',"GL4-1");
+    ga_mouse_click_event('#end-month',"GL4-1");
+    
     //GL5-2  圖片
     ga_mouse_over_event('.media-switch-blk',"GL5-2")
 
