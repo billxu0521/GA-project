40f3a5a4a1363a70d0fcbd366cab5b98c8e52342
billxu <billxu0521@gmail.com>
2017-03-29 16:50:00 +0800

    no message

--- ga_inject_lib.js
+++ ga_inject_lib.js
@@ -251,7 +251,7 @@ window.ga_mouse_down_event = function (_selector, _event_type, _name) {
  */
 window.ga_mouse_touch_event = function (_selector, _event_type, _name) {
     var _event_key = 'touch';
-      $(_selector).touchwipe(function () {
+      $(_selector).on("click",function () {
           _name = _get_element_name(this, _selector, _name);
 
           _console_log([_event_type, _name, _event_key]);
