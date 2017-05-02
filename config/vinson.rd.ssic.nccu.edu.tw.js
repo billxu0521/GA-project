2ed6f38142f3773f4c99c860e5f9fdd4210425e2
billxu <billxu0521@gmail.com>
2017-05-02 09:49:27 +0800

    數位人文平台

---
+++ config/vinson.rd.ssic.nccu.edu.tw.js
@@ -0,0 +1,34 @@
+/**
+ * 適用網頁：http://vinson.rd.ssic.nccu.edu.tw/
+ * 事件查詢表：https://docs.google.com/spreadsheets/d/1MtMtw9lKLDTUzfBd6Ld0fAe_FGe5u-Mlkh5WfZiH5qM/edit
+ * @author Pudding 20170203
+ */
+
+GA_TRACE_CODE = "UA-89833109-4";
+
+var _local_debug = false;
+
+if (_local_debug === true) {
+    //CSS_URL = "https://localhost/GA-project/config/www.travel.taipei.css";
+    LIB_URL = "https://localhost/GA-project/ga_inject_lib.js";
+    console.log("[LOCAL TEST MODE]");
+}
+else {
+    //CSS_URL = "https://billxu0521.github.io/GA-project/config/www.travel.taipei.css";
+    LIB_URL = "https://billxu0521.github.io/GA-project/ga_inject_lib.js";
+}
+
+
+var exec = function () {
+    
+};
+
+// --------------------------------------
+
+$(function () {
+    $.getScript(LIB_URL, function () {
+        ga_setup(function () {
+            exec();
+        });
+    });
+});
