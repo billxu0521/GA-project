6fea7c6e57f747b00b986453e16440e387e68e29
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-05-26 14:45:43 +0800

    0526 全景

--- config/www.travel.taipei.js
+++ config/www.travel.taipei.js
@@ -152,7 +152,7 @@ var exec = function () {
     //GL5-3  互動
     ga_mouse_click_event('.spot-photo-slider .flickity-prev-next-button',"GL5-3");
     ga_mouse_click_event('.spot-photo-slider .flickity-lazyloaded',"GL5-3");
-    ga_mouse_drag_event('#krpanoSWFObject',"GL5-3");
+    ga_mouse_down_event('#krpanoSWFObject canvas',"GL5-3");
     
 
     //ga_mouse_click_event('.media-switch-blk',"GL5-3");
