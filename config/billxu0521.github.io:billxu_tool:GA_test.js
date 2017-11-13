c6e09e3cf1affda2bac1cb42f80e5d16cfdbe4f0
billxu <billxu0521@gmail.com>
2017-11-13 22:45:58 +0800

    上課用

---
+++ config/billxu0521.github.io:billxu_tool:GA_test.js
@@ -0,0 +1,39 @@
+/**
+ * 適用網頁：http://vinson.rd.ssic.nccu.edu.tw/
+ * 事件查詢表：https://docs.google.com/spreadsheets/d/1MtMtw9lKLDTUzfBd6Ld0fAe_FGe5u-Mlkh5WfZiH5qM/edit
+ * @author Pudding 20170203
+ */
+
+GA_TRACE_CODE = "UA-89833109-5";
+
+var _local_debug = false;
+
+if (_local_debug === true) {
+    CSS_URL = "https://localhost/GA-project/config/billxu0521.github.io/billxu_tool/GA_test.css";
+    LIB_URL = "https://localhost/GA-project/ga_inject_lib.js";
+    console.log("[LOCAL TEST MODE]");
+}
+else {
+    CSS_URL = "https://billxu0521.github.io/GA-project/config/billxu0521.github.io/billxu_tool/GA_test.css";
+    LIB_URL = "https://billxu0521.github.io/GA-project/ga_inject_lib.js";
+}
+
+
+var exec = function () {
+    auto_set_user_id();   
+
+    //搜尋按鈕
+    ga_mouse_click_event(".btn","Click");
+    
+};
+
+
+// --------------------------------------
+
+$(function () {
+    $.getScript(LIB_URL, function () {
+        ga_setup(function () {
+            exec();
+        });
+    });
+});
