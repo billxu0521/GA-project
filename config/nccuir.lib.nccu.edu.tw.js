aa657abd2d26502814599c06efad6347396e348d
JinsonChang <ccsheng@gmail.com>
2019-05-16 15:18:19 +0800

    Update nccuir.lib.nccu.edu.tw.js

--- config/nccuir.lib.nccu.edu.tw.js
+++ config/nccuir.lib.nccu.edu.tw.js
@@ -25,11 +25,14 @@ var exec = function () {
     //偵測語法
     //ga_mouse_click_event("選擇要素","準則");
     ga_mouse_click_event(".btn","Click");
+    ga_mouse_click_event("[input]", "Click", function (_ele) {
+        return _ele.text();});
     ga_mouse_click_event(".btn gsc-search-button gsc-search-button-v2","Click"); //GOOGLE站內搜尋
     ga_mouse_click_event(".btn scholar_href","Click");
+    ga_mouse_click_event(".btn mouse_clickClick","Click"); //首頁「統計資訊-更多...>>」、「學術動態-更多...>>」…首頁的眾多滑鼠事件
+    ga_mouse_click_event(".btn scholar_href mouse_clickClick","Click");
+    ga_mouse_click_event(".btn scholar_href","Click");
     ga_mouse_click_event(".btn btn-info btn-lg mouse_clickClick","Click"); //首頁「研究/學術產出」搜尋按鈕
-    //ga_input_change_event("選擇要素","準則");
-    ga_input_change_event(".input","Input");
     ga_mouse_click_event("[href]", "Click", function (_ele) {
         return _ele.text();});
     ga_submit_event("form", "Form", function (_ele) {
