dcc6a628e83b622fc28f03f1a26e6c9ef405f62c
billxu <billxu0521@gmail.com>
2019-12-06 12:16:16 +0800

    no message

--- config/nccuir.lib.nccu.edu.tw.js
+++ config/nccuir.lib.nccu.edu.tw.js
@@ -60,9 +60,10 @@ var exec = function () {
     ga_mouse_click_event('a.scholar_href','Home_Browse_Dinstinguished Scholar');
     ga_mouse_click_event('#academic_trends a','Home_Information_Academic Trends');
     ga_mouse_click_event('#statisticail_info a','Home_Information_Statistical Data');
-
+console.log($('#researcher_list div.col-lg-9 ul li a'));
     //研究者
     ga_mouse_click_event('#researcher_list div.col-lg-9 ul li a','Researcher List_Browse');
+    ga_mouse_click_event('#researcher_list div.col-lg-9 ul li ul li a','Researcher List_Browse');
     ga_mouse_click_event('#researcher_list div.col-lg-3 div nav ui ul li','Researcher List_Browse');
     ga_mouse_click_event('#researcher_list div.col-lg-9 div h2 a','Researcher List_Search');
     ga_mouse_click_event('#statisticail_info a','Researcher List_Browse');
